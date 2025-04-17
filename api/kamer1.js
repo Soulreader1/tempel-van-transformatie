a3641ac610383da44436122cca9f0346ab3e3446
Soulreader1 <chris300776@hotmail.com>
2025-04-17 20:48:08 +0200

    Create kamer1.js

---
+++ api/kamer1.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    kamer: 1,
+    welkom: "Welkom in Kamer 1 van de Tempel van Transformatie",
+    status: "actief"
+  });
+}
