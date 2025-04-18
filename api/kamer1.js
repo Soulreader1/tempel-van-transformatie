f77319d6f803c5712d2138c411fd0870f487636a
Soulreader1 <chris300776@hotmail.com>
2025-04-19 01:09:55 +0200

    Update kamer1.js

--- api/kamer1.js
+++ api/kamer1.js
@@ -1,5 +1,5 @@
+// /api/kamer1.js
 export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // <<< DIT LAAT ALLES TOE
   res.status(200).json({
     kamer: 1,
     welkom: "Welkom in Kamer 1 van de Tempel van Transformatie",
